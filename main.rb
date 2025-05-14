dafdd10a4a97bdacafeaaa255f1f151245c8ffe6
fum1ple <fumiya.n0702@gmail.com>
2025-05-14 16:56:25 +0900

    [add]出力処理実装

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require_relative 'lib/csv_loader'
 require_relative 'lib/sales_aggregator'
+require_relative 'output/result_exporter'
 
 data_dir = "data"
 
@@ -8,4 +9,12 @@ file_paths = Dir.glob("#{data_dir}/*_売上.csv")
 # CSVを読み込む
 sales_data = CsvLoader.load_all(file_paths)
 
-puts SalesAggregator.aggregate(sales_data)
+organized_sales_data = SalesAggregator.aggregate(sales_data)
+
+begin
+  ResultExporter.export_csv(organized_sales_data)
+rescue StandardError => e
+  puts "Error exporting CSV: #{e.message}"
+  exit 1
+end
+puts "Sales data has been successfully aggregated and exported."
